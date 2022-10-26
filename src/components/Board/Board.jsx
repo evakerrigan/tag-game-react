e934002a3eedb1fd86dd27122317727f99c9e4c4
Eva Kerrigan <evakerrigan@gmail.com>
2022-10-26 19:06:43 +0300

    feat: new init 5

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,9 +1,6 @@
 import { React, Component } from "react";
 import Element from '../Element/Element.jsx';
 
-  let arrTag = [4, 6, 0, 7, 5, 1, 8, 2, 3];
-  let tagNumber = 3;
-  let matrixArray = [];
 
   class Board extends Component {
     render() {
@@ -14,10 +11,6 @@ import Element from '../Element/Element.jsx';
           <Element />
 
         </div>
-
-
-
-
 
 
         )
