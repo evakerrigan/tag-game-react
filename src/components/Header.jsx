030c1e5ef437e2ac1c4854f69622a46be42ea8ce
Eva Kerrigan <evakerrigan@gmail.com>
2022-11-19 14:38:32 +0300

    refactor: clean header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,7 @@
-export const Header = ({time, movie}) => {
+export const Header = ({
+  time, movie,
+  newGame = () => undefined
+}) => {
 
 const minutes = (time/60)|0;
 const seconds = time%60;
@@ -6,27 +9,19 @@ const seconds = time%60;
   return (
     <div>
     <h1 className="title">Game Tag</h1>
-
-
     <div className="wrapper">
-
       <div className="no-button">Time</div>
-
-
       <div className="time">
         <span id="minutes">{minutes} :</span>
         <span id="seconds">{seconds}&nbsp;</span>
       </div>
-
       <div className="no-button">Movie</div>
       <div className="movie-count">{movie}</div>
       <div className="audio-button"></div>
-
     </div>
 
     <div className="wrapper buttons">
-
-      <div className="button" onclick="newGame()">New Game</div>
+      <div className="button" onClick={() => newGame()}>New Game</div>
       <div className="button save">Save</div>
       <div className="button load">Load</div>
       <div className="button top">Top results</div>
